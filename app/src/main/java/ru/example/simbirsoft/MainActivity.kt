efd6b5ae0f85e8637070fc4783ada4f85f5b98da
ag <harrikein73@gmail.com>
2018-02-14 13:35:45 +0400

    Стандартный Firebase логин экран

--- app/src/main/java/ru/example/simbirsoft/MainActivity.kt
+++ app/src/main/java/ru/example/simbirsoft/MainActivity.kt
@@ -4,7 +4,10 @@ import android.app.Activity
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,7 +18,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val provides = listOf<AuthUI.IdpConfig>()
+        val provides =
+                listOf<AuthUI.IdpConfig>(AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build())
 
         startActivityForResult(AuthUI.getInstance()
                 .createSignInIntentBuilder()
@@ -23,16 +27,25 @@ class MainActivity : AppCompatActivity() {
                 .build(), SIGN_IN_REQ)
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
         super.onActivityResult(requestCode, resultCode, data)
+        val isResultOk = resultCode == Activity.RESULT_OK
         when(requestCode) {
             SIGN_IN_REQ -> {
-                if (resultCode == Activity.RESULT_OK) {
-
+                if (isResultOk) {
+                    FirebaseAuth.getInstance().currentUser?.let {
+                        showToast("Hello ${it.displayName}")
+                    }
                 } else {
-
+                    IdpResponse.fromResultIntent(data)?.let {
+                        showToast("Error ${it.errorCode} for login user ${it.email}")
+                    }
                 }
             }
         }
+    }
+
+    private fun showToast(text: CharSequence) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
     }
 }
